440f27cbccf0f78c112d0436c6565ef7a455d44b
agulbs <agulbenkian@agulb-win10.castle.local>
2022-08-11 15:15:45 -0400

    need to fix passwords and security

--- dashboard/src/app/components/homepage/pricing/pricing.component.ts
+++ dashboard/src/app/components/homepage/pricing/pricing.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RequestsService } from '../../../services/requests.service'
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.css']
 })
 export class PricingComponent implements OnInit {
-
-  constructor() { }
+  public tiers = [];
+  constructor(private _requests: RequestsService) { }
 
   ngOnInit(): void {
+    this._requests.getRequest("tiers").subscribe(res => {
+      if (res['status'] == 200) {
+        this.tiers = res['message']
+      }
+    })
   }
 
 }
